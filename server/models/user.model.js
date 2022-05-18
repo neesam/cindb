f419576309fe4a24d96c4b4100dc53897bba970c
derekhongg <hongderek@gmail.com>
2022-05-18 15:41:53 -0700

    Derek Update

---
+++ server/models/user.model.js
@@ -0,0 +1,60 @@
+const mongoose = require("mongoose");
+const bcrypt = require('bcrypt');
+
+const UserSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: [true, "First name is required"]
+    },
+    lastName: {
+        type: String,
+        required: [true, "Last name is required"]
+    },
+    userName: {
+        type: String,
+        required: [true, "Username is required"]
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        validate: {
+            validator: val => /^([\w-\.]+@([\w-]+\.)+[\w-]+)?$/.test(val),
+            message: "Please enter a valid email"
+        }
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+        minlength: [8, "Password must be 8 characters or longer"]
+    }
+}, {timestamps: true});
+
+//this will confirm the password for the model
+UserSchema.virtual('confirmPassword')
+    .get( () => this._confirmPassword )
+    .set( value => this._confirmPassword = value );
+
+UserSchema.pre('validate', function(next) {
+        if (this.password !== this.confirmPassword) {
+            this.invalidate('confirmPassword', 'Password must match confirm password');
+            console.log("Passwords do not match.")
+        }
+        next();
+        });
+
+//Will save the user
+UserSchema.pre('save', function(next) {
+    //hash the password before saving it
+    bcrypt.hash(this.password, 10)
+        .then(hash => {
+            this.password = hash;
+            next();
+        })
+        .catch(err => {
+            console.log('Error saving hash');
+            console.log(err);
+            })
+        });
+
+        const User = mongoose.model('User', UserSchema);
+module.exports = User;
